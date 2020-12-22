25b9099652a64638d130467aa1071834c09d29dd
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-12-22 06:13:03 -0500

    Actualiza

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -16,6 +16,8 @@ module Dummy
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.load_defaults 6.1
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'America/Bogota'

bbe35a1b441b4d42d0c0d06f3512bfa5fceb6b47
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-04-22 21:27:51 -0500

    Actualiza

--- test/dummy/db/structure.sql
+++ test/dummy/db/structure.sql
@@ -424,7 +424,8 @@ CREATE TABLE public.sip_actorsocial_persona (
     perfilactorsocial_id integer,
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL,
-    correo character varying(100)
+    correo character varying(100),
+    cargo character varying(254)
 );
 
 
@@ -2229,6 +2230,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20190401175521'),
 ('20190612111043'),
 ('20190618135559'),
+('20190625112649'),
 ('20190715083916'),
 ('20190715182611'),
 ('20190818013251'),
@@ -2247,6 +2249,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20200919003430'),
 ('20201119125643'),
 ('20210401194637'),
-('20210401210102');
+('20210401210102'),
+('20210414201956');
 
 
